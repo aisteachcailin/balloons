6127637653766c85d606a16f30f73cab4efa3e53
AndreyBan <boriandr@bk.ru>
2024-03-25 21:42:54 +0300

    fix: validate

--- src/js/registration.js
+++ src/js/registration.js
@@ -14,12 +14,12 @@
      */
     function displayTypeForm() {
         const typeForm = document.querySelector('.js-type-form')
-        const radioTypeForm = typeForm.querySelectorAll('input[type=radio]')
+        const radioTypeForm = typeForm.querySelectorAll('input[name="form-registration"][type=radio]')
 
 
         radioTypeForm.forEach(el => {
             el.addEventListener('change', () => {
-                const valueTypeForm = typeForm.querySelector('input[type=radio]:checked').value
+                const valueTypeForm = typeForm.querySelector('input[name="form-registration"][type=radio]:checked').value
 
                 typeForm.setAttribute('data-form-type', valueTypeForm)
 
